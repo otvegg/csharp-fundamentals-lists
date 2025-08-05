b91d07b16693ecd1475c1a89406525237adb0aa8
Oyvind Timian Dokk Husveg <oyvind.timian.dokk.husveg@no.experis.com>
2025-08-05 11:18:42 +0200

    Oyvind Husveg

--- csharp-fundamentals-lists.Main/Core.cs
+++ csharp-fundamentals-lists.Main/Core.cs
@@ -19,7 +19,7 @@ namespace csharp_fundamentals_lists.Main
 
         public Core()
         {
-            
+
         }
 
         public List<string> Question1()
@@ -31,26 +31,31 @@ namespace csharp_fundamentals_lists.Main
             //  TODO:  1.  Find the add method and add two more flavours of ice cream: "Phish Food", "Peanut Butter Cup"
 
             //write code here
+            _iceCreams.Add("Phish Food");
+            _iceCreams.Add("Peanut Butter Cup");
 
-            return _iceCreams;        
+
+
+            return _iceCreams;
         }
 
         public int Question2()
         {
-            
+
             //TODO:  find the lists method that returns the number of ice creams in the list and return this.
 
-            // remove exception and write code here
-            throw new NotImplementedException();
+            return _iceCreams.Count;
         }
         public List<string> Question3()
         {
 
             // The code below concatenates this.MoreIceCream to the _iceCreams list into a new results list.
-            //TODO: you can 'chain' methods on the _iceCream list, so add another Concat to include EvenMoreIceCream  (this is defined below) to the result list . e.g.   _iceCreams.Concat(this.MoreIceCream).Concat(other list to concat).ToList()
+            // TODO: you can 'chain' methods on the _iceCream list, so add another Concat to
+            // include EvenMoreIceCream  (this is defined below) to the result list . e.g.
+            // _iceCreams.Concat(this.MoreIceCream).Concat(other list to concat).ToList()
 
-            List<string> results = _iceCreams.Concat(this.MoreIceCream).ToList();
-            
+            List<string> results = _iceCreams.Concat(this.MoreIceCream).Concat(this.EvenMoreIceCream).ToList();
+
             return results;
 
             // remove exception and write code here
@@ -59,14 +64,14 @@ namespace csharp_fundamentals_lists.Main
         public List<string> Question4()
         {
 
-
             //TODO: Remove the duplicates using the .Distinct() placing just before the .ToList()
-            //      copy the List declaration line from Question3 and add the .Distinct() into the chain.  e.g.  _iceCreams.Concat(this.MoreIceCream).Concat(other list to concat).Distinct().ToList()
+            //      copy the List declaration line from Question3 and add the .Distinct() into the chain.
+            //      e.g.  _iceCreams.Concat(this.MoreIceCream).Concat(other list to concat).Distinct().ToList()
             //      be sure to include the MoreIceCream and EvenMoreIceCream lists
 
 
-            List<string> results = _iceCreams;
-            // remove exception and write code here
+            List<string> results = _iceCreams.Concat(this.MoreIceCream).Concat(this.EvenMoreIceCream).Distinct().ToList();
+
             return results;
         }
 
